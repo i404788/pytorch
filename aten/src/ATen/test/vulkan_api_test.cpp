6c8c102b2aec00eb70c1215891ff9a8e1cd614cd
Ashkan Aliabadi <ashkanaliabadi@fb.com>
2020-10-16 16:10:05 -0700

    Add Vulkan op Add.
    ghstack-source-id: 4948c979ca26966ba65696ad88cd383fda4900a4
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/44017

--- aten/src/ATen/test/vulkan_api_test.cpp
+++ aten/src/ATen/test/vulkan_api_test.cpp
@@ -31,6 +31,21 @@ TEST(VulkanAPITest, copy) {
   }
 }
 
+TEST(VulkanAPITest, add) {
+  if (!at::native::vulkan::api::available()) {
+    return;
+  }
+
+  const auto a_cpu = at::rand({1, 2, 2, 3}, at::device(at::kCPU).dtype(at::kFloat));
+  const auto b_cpu = at::rand({1, 2, 2, 3}, at::device(at::kCPU).dtype(at::kFloat));
+  const auto c_cpu = at::add(a_cpu, b_cpu, 2);
+  const auto a_vulkan = a_cpu.vulkan();
+  const auto b_vulkan = b_cpu.vulkan();
+  const auto c_vulkan = at::add(a_vulkan, b_vulkan, 2);
+
+  ASSERT_TRUE(almostEqual(c_cpu, c_vulkan.cpu()));
+}
+
 } // namespace
 
 #endif /* USE_VULKAN_API */
